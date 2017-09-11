89776d1e34fdc61b22205755e14667c38933a991
François Beaulieu <francois@simbioz.com>
2017-09-11 16:24:51 -0400

    Add demo app to test vlc player and anything else.

---
+++ VideoPlayerTest/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace VideoPlayerTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaUriElement.Stop();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            var result = dlg.ShowDialog();
+            if (result == true)
+                mediaUriElement.LoadMedia(dlg.FileName);
+            mediaUriElement.Play();
+        }
+    }
+}
